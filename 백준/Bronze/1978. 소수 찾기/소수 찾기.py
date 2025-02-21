1bd96ebf5d798844e0aa7810e1ff56317e18f0ec
Jintonic92 <86962114+Jintonic92@users.noreply.github.com>
2025-02-21 14:26:31 +0900

    [Bronze II] Title: 소수 찾기, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.py
@@ -0,0 +1,13 @@
+nothing = input()
+bin = list(map(int, input().split()))
+
+def is_prime(num):
+  if num < 2:
+    return False 
+  else:
+    for i in range(2, int(num**0.5)+1):
+      if num % i == 0 :
+        return False
+  return True
+
+print(sum(list(map(is_prime, bin))))
