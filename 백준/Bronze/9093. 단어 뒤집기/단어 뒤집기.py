2ca189d7fe6d69bfc215ebb4f41f3ae4cbce0dd9
Jintonic92 <86962114+Jintonic92@users.noreply.github.com>
2025-03-14 10:55:14 +0900

    [Bronze I] Title: 단어 뒤집기, Time: 1124 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.py
+++ 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.py
@@ -1,18 +1,21 @@
-import sys
+# 9093 단어 뒤집기
 
 n = int(input())
-
 for _ in range(n):
-  sentence = input()
-  bin = []
-  #sentence = "I am happy today"
-  for each in sentence.split():
-    bin.append(each)
-    bin.append(" ")
-  for i in range(len(bin)):
-    if bin[i] == "":
-      print(" ", end ="")
-    else:
-      for j in range(len(bin[i])-1, -1, -1):
-        print(bin[i][j], end= "")
+    sent = input()
+    bin = []
+    
+    for each in sent.split():
+        bin.append(each)
+        bin.append(" ")
+        
+    for i in range(len(bin)):
+        if bin[i] == " ":
+            print(" ", end = "")
+        else:
+            for j in range(len(bin[i])-1, -1, -1):
+                print(bin[i][j], end = "")
+
+
+  
 
