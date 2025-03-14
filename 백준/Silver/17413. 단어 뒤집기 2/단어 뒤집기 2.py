51afe28671e1b48978d62fb27b8e7737bd2a2082
Jintonic92 <86962114+Jintonic92@users.noreply.github.com>
2025-03-14 11:29:41 +0900

    [Silver III] Title: 단어 뒤집기 2, Time: 208 ms, Memory: 35548 KB -BaekjoonHub

--- 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.py
+++ 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.py
@@ -1,37 +1,33 @@
-# 17413 : 단어 뒤집기 2
-# 3가지 경우 : 빈칸, 단어, <> 
-
-# deque로 왼쪽부터 하나씩 뽑기 3가지 경우 비교
-
 from collections import deque
 
+# 문자열, <>, 빈칸 3가지 경우
+# bin에 임시방편으로 넣어놓고 프린트하도록 설정
+
 sent = deque(input())
-result = "" # 프린트 결과
-bin = [] # 임시list
+bin = []
+result = ""
 
-while sent: 
-  x = sent.popleft() # 첫번째 문자열
-
-  if x == " ":
-    if bin :
-      result += "".join(bin[::-1]) # bin result에 추가
-      bin = [] # 비워주기
-    result += x
-  
-  elif x == "<":
-    if bin :
-      result += "".join(bin[::-1])
-      bin = []
-    result += x
-    while sent and x != '>': # > 만날때까지
-      x = sent.popleft()
-      result += x
+while sent :
+    x = sent.popleft()
     
-  else: # 문자열일 경우
-    bin.append(x)
-
-if bin: # 아직 남아 있다면
-  result += ''.join(bin[::-1])
-
+    if x == " ":
+        if bin :
+            result += "".join(bin[::-1])
+            bin = [] # 비워주기
+        result += x
+    
+    elif x == "<":
+        if bin :
+            result += "".join(bin[::-1])
+            bin = [] # 비워주기
+        result += x
+        while sent and x != ">":
+            x = sent.popleft()
+            result += x
+    else:
+        bin.append(x)
+        
+if bin:
+    result += "".join(bin[::-1])
+    
 print(result)
-
