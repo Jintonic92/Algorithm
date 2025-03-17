b6c9bfd926f4cd9f2f4feacbbb8f69964e6a993a
Jintonic92 <86962114+Jintonic92@users.noreply.github.com>
2025-03-17 20:33:37 +0900

    [Bronze I] Title: 단어 뒤집기, Time: 1104 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.py
+++ 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.py
@@ -1,21 +1,18 @@
-# 9093 단어 뒤집기
+# 9093 : 단어 뒤집기
+# 단어가 들어오고 2개 이상이면 뒤집어서 출력
 
 n = int(input())
 for _ in range(n):
-    sent = input()
-    bin = []
-    
-    for each in sent.split():
-        bin.append(each)
-        bin.append(" ")
-        
-    for i in range(len(bin)):
-        if bin[i] == " ":
-            print(" ", end = "")
-        else:
-            for j in range(len(bin[i])-1, -1, -1):
-                print(bin[i][j], end = "")
+  sent = input()
+  stack = []
+  for each in sent.split():
+    stack.append(each)
+    stack.append(" ")
 
-
-  
+  for each in stack:
+    if each == " " or len(each) == 1:
+      print(each, end = "")
+    else:
+      for i in range(len(each)-1, -1, -1):
+        print(each[i], end= "")
 
