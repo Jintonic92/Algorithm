a00731954158be7913975a28d9fac31bd44e148c
Jintonic92 <86962114+Jintonic92@users.noreply.github.com>
2025-03-17 20:42:33 +0900

    [Silver III] Title: 단어 뒤집기 2, Time: 204 ms, Memory: 35548 KB -BaekjoonHub

--- 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.py
+++ 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.py
@@ -1,33 +1,36 @@
+# 17143 : 단어뒤집기2
+
+# <>, "", 단어 3가지 경우
+# <은 >이 나올때까지 그대로 
+# 그 외는 뒤집어서 bin에 넣기
+# 공백도 그대로
+
 from collections import deque
-
-# 문자열, <>, 빈칸 3가지 경우
-# bin에 임시방편으로 넣어놓고 프린트하도록 설정
-
-sent = deque(input())
-bin = []
+queue = deque(input())
 result = ""
+bin = []
 
-while sent :
-    x = sent.popleft()
-    
-    if x == " ":
-        if bin :
-            result += "".join(bin[::-1])
-            bin = [] # 비워주기
-        result += x
-    
-    elif x == "<":
-        if bin :
-            result += "".join(bin[::-1])
-            bin = [] # 비워주기
-        result += x
-        while sent and x != ">":
-            x = sent.popleft()
-            result += x
-    else:
-        bin.append(x)
-        
+while queue:
+  x = queue.popleft()
+
+  if x == " ":
+    if bin :
+      result += "".join(bin[::-1])
+      bin = []
+    result += x
+  
+  elif x == "<":
+    if bin:
+      result += "".join(bin[::-1])
+      bin = []
+    result += x
+    while x != ">":
+      x = queue.popleft()
+      result += x
+  
+  else:
+    bin.append(x)
+
 if bin:
-    result += "".join(bin[::-1])
-    
+  result += "".join(bin[::-1])
 print(result)
