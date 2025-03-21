1b4b12636c68ba1765b2b34416bf8a6e1ca1ae72
Jintonic92 <86962114+Jintonic92@users.noreply.github.com>
2025-03-21 13:22:43 +0900

    [Bronze II] Title: 문자열 분석, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10820. 문자열 분석/문자열 분석.py
+++ 백준/Bronze/10820. 문자열 분석/문자열 분석.py
@@ -1,19 +1,12 @@
-while True:
+while True :
   try : 
-    sent = input()
-    is_lower = 0
-    is_upper = 0 
-    is_digit = 0
-    is_space = 0  
+    sent =  input()
+    is_lower, is_upper, is_digit, is_space = 0, 0, 0, 0
     for each in sent:
-      if each.islower():
-        is_lower += 1
-      elif each.isupper():
-        is_upper += 1
-      elif each.isdigit():
-        is_digit += 1
-      elif each ==" ":
-        is_space +=1 
+      if each.islower(): is_lower += 1
+      elif each.isupper() : is_upper += 1
+      elif each.isdigit() : is_digit += 1
+      else : is_space += 1
 
     print(is_lower, is_upper, is_digit, is_space)
   except EOFError:
