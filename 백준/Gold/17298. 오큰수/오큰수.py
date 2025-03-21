b7040e2437bc9e842a61ce60acde64c4ccce64b9
Jintonic92 <86962114+Jintonic92@users.noreply.github.com>
2025-03-21 11:35:11 +0900

    [Gold IV] Title: 오큰수, Time: 1056 ms, Memory: 138800 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.py
+++ 백준/Gold/17298. 오큰수/오큰수.py
@@ -1,16 +1,11 @@
-# 17298 : 오큰수
-from collections import deque
-
 n = int(input())
-a = list(map(int, input().split()))
-
-result = [-1] * n # 오큰수 저장 배열
-stack = [] # 인덱스 저장 스택
+inp_list = list(map(int, input().split()))
+stack = [] # 인덱스 저장
+result = [-1 for _ in range(n)]
 
 for i in range(n):
-  # stack이 비어있지 않고, 현재 원소가 stack 맨 위 원소보다 크면
-  while stack and a[stack[-1]] < a[i]:
-    result[stack.pop()] = a[i]
-  stack.append(i)
-
+    while stack and inp_list[stack[-1]] < inp_list[i]:
+        result[stack.pop()] = inp_list[i]
+    stack.append(i)
+    
 print(*result)
