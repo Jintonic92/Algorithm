c8d8b71f34a2a2a855cac6d7f184a74a55a34e66
Jintonic92 <86962114+Jintonic92@users.noreply.github.com>
2025-03-24 12:59:20 +0900

    [Silver III] Title: 후위 표기식2, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1935. 후위 표기식2/후위 표기식2.py
+++ 백준/Silver/1935. 후위 표기식2/후위 표기식2.py
@@ -1,34 +1,34 @@
-# 1935 : 후위표기식2
+# 1935 : 후외표기식
 
 n = int(input())
 equa = input()
 stack = []
+
 for _ in range(n):
   stack.append(int(input()))
-
-for each in equa:
-  #print("each", each)
-  if each.isalpha():
-    each = stack[ord(each) - 65]
-    stack.append(each)
   
+for each in equa:
+  if each.isalpha():
+    num = stack[ord(each) - 65]
+    stack.append(num)
+    #print(stack)
+
   else:
     a = stack.pop()
-    result = stack.pop()
-    #print(a, result)
+    b = stack.pop()
 
-    if each == "+":
-      result += a
-    
-    elif each == "*":
-      result *= a
-    
+    if each == "*":
+      b *= a
+      
+    elif each == "+":
+      b += a
+      
     elif each == "/":
-      result /= a
-
-    elif each == "-":
-      result -= a
+      b /= a
     
-    stack.append(result)
+    elif each == "-":
+      b -= a
+    
+    stack.append(b)
 
 print("%.2f" %stack[-1])
