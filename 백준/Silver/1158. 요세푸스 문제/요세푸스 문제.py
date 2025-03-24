b994c787f6ad918f548e1d21cdd68b376b414f77
Jintonic92 <86962114+Jintonic92@users.noreply.github.com>
2025-03-24 11:02:50 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 2268 ms, Memory: 34908 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
@@ -1,13 +1,11 @@
-# 단일 deque
-from collections import deque
-
 n, k = map(int, input().split())
-queue = deque(range(1, n+1))
+from collections import deque
+queue = deque(x for x in range(1, n+1))
 result = []
 
-while queue :
-  for _ in range(k -1):
-    queue.append(queue.popleft())
-  result.append(queue.popleft())
+while queue:
+    for _ in range(k-1):
+        queue.append(queue.popleft())
+    result.append(queue.popleft())
 
 print("<" + ", ".join(map(str, result)) + ">")
