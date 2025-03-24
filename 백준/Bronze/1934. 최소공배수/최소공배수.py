105086f0b6e4db58caed41a03229f4c109302d5f
Jintonic92 <86962114+Jintonic92@users.noreply.github.com>
2025-03-24 13:33:09 +0900

    [Bronze I] Title: 최소공배수, Time: 92 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/1934. 최소공배수/최소공배수.py
+++ 백준/Bronze/1934. 최소공배수/최소공배수.py
@@ -1,8 +1,18 @@
-# 1934 : 최소공배수
-import math 
+# 1934: 최소공배수
+def gcd(x, y):
+  x, y = max(x, y), min(x, y)
+  while y > 0:
+    x, y = y, x % y
+  return x
+
+def lcm(x, y):
+  x, y = max(x, y), min(x, y)
+  gcd_result = gcd(x, y)
+  #print(gcd_result)
+  return x * y / gcd_result
 
 n = int(input())
 for _ in range(n):
-  a, b = map(int, input().split())
-
-  print(math.lcm(a, b))
+  x, y = map(int, input().split())
+  print(int(lcm(x, y)))
+  
