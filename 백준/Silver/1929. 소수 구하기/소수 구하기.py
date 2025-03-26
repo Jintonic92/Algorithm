2efae4cf177743aae1a21fffa5f747f5601bc681
Jintonic92 <86962114+Jintonic92@users.noreply.github.com>
2025-03-26 12:26:52 +0900

    [Silver III] Title: 소수 구하기, Time: 3316 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.py
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -1,14 +1,15 @@
-# 1929 : 소수 구하기
+#1929 : 소수구하기
+
+a, b = map(int, input().split())
+
 def is_prime(x):
-  if x < 2:
-    return False
-  else:
-    for i in range(2, int(x**0.5)+1):
-      if x % i == 0:
+    if x < 2:
         return False
+    for i in range(2, int(x**0.5)+1):
+        if x % i == 0:
+            return False
     return True
 
-n, m = map(int, input().split())
-for x in range(n, m+1):
-  if is_prime(x):
-    print(x)
+for i in range(a, b+1):
+    if is_prime(i):
+        print(i)
