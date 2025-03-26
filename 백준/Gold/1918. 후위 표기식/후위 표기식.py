e05a204c2fdbcae00af6974a42a9df64355bcce3
Jintonic92 <86962114+Jintonic92@users.noreply.github.com>
2025-03-26 12:11:09 +0900

    [Gold II] Title: 후위 표기식, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/1918. 후위 표기식/후위 표기식.py
+++ 백준/Gold/1918. 후위 표기식/후위 표기식.py
@@ -1,37 +1,44 @@
-# 1918 : 후위표기식 
+# 1918 : 후위 표기식
 
-equa = input()
-stack = []
-res = ""
+# 우선순위 (), */, -+
+# 결과에 알파벳 넣고 
+# 우선순위 대로 처리
+# (은 일단 추가 
+# )은 (이 나올떄까지 bin에 넣고
+  # bin 초기화
+  # ( 뽑기
+# +- 은 bin[-1] = (이 아닐떄까지 추가 
+# */  은 bin[-1] = * 또는 / 일때까지 추가
 
-for each in equa:
+sent = input()
+res = ''
+bin = []
+
+for each in sent:
   if each.isalpha():
     res += each
-  
   else:
-    # 우선순위 1
-    if each == "(":
-      stack.append(each)
-    
+    if each == '(':
+      bin.append(each)
+      
+
     elif each == ")":
-      while stack and stack[-1] != "(":
-        res += stack.pop()
-      stack.pop() # "(" 제거
-
-    # 우선순위 2
+      while bin and bin[-1] != "(":
+        res += bin.pop()
+      bin.pop()
+    
     elif each == "*" or each == "/":
-      while stack and (stack[-1] == "*" or stack[-1] == "/"):
-        res += stack.pop()
-      stack.append(each)
-
-    # 우선순위 3
+      
+      while bin and (bin[-1] == "*" or bin[-1] == "/"):
+        res += bin.pop()
+      bin.append(each)
+    
     elif each == "+" or each == "-":
-      while stack and stack[-1] != "(":
-        res += stack.pop()
-      stack.append(each)
+      while bin and bin[-1] != "(":
+        res += bin.pop()
+      bin.append(each)
 
-
-while stack :
-  res += stack.pop()
+while bin:
+  res += bin.pop()
 
 print(res)
