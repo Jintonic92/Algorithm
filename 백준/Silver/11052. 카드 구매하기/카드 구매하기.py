d7375daedc4eeeedce32cbdd18e6c261330d4918
Jintonic92 <86962114+Jintonic92@users.noreply.github.com>
2025-04-16 14:19:47 +0900

    [Silver I] Title: 카드 구매하기, Time: 160 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/11052. 카드 구매하기/카드 구매하기.py
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.py
@@ -1,14 +1,17 @@
-# 11052 카드 구매하기
+# 카드 구매하기
 
-# dp[i]와 dp[i-j] + 카드리스트[j] 중에서 더 큰 값이 
-# i개를 사는 최대값
+# dp 를 만들고 
+# 1부터 N까지 i 와 n-i 의 합으로 구한다 
 
 n = int(input())
 inp_list = list(map(int, input().split()))
-dp = list(0 for _ in range(n+1))
+dp = [0] + inp_list + [0 for _ in range(n)]
 
-for i in range(1, n+1):
-  for j in range(1, i+1):
-    dp[i] = max(dp[i], dp[i-j] + inp_list[j-1])
+for i in range(n+1):
+  for j in range(i):
+    #print(f"dp[{i}] = max(dp[{i}], dp[{i-j}] + dp[{j}])")
+    dp[i] = max(dp[i], dp[i-j] + dp[j])
+    #print(dp)                  
 
 print(dp[n])
+
