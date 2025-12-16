e02640e245b9c9070ad69015a7a08ac3bc412c4e
Jintonic92 <86962114+Jintonic92@users.noreply.github.com>
2025-12-16 22:50:44 +0900

    [level 4] Title: 입양 시각 구하기(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
@@ -0,0 +1,15 @@
+-- 코드를 입력하세요
+
+# SET @hour := -1; -- 변수 선언
+
+# SELECT (@hour := @hour + 1) as HOUR,
+# (SELECT COUNT(*) FROM ANIMAL_OUTS WHERE HOUR(DATETIME) = @hour) as COUNT
+# FROM ANIMAL_OUTS
+# WHERE @hour < 23
+
+SET @HOUR := -1 ;
+
+SELECT (@HOUR := @HOUR + 1) AS HOUR
+    , (SELECT COUNT(*) FROM ANIMAL_OUTS WHERE HOUR(DATETIME) = @HOUR) AS COUNT
+FROM ANIMAL_OUTS
+WHERE @HOUR < 23
