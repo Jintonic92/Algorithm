adcbd6a71d2b68675fef42acc65c467d2a6c7700
Jintonic92 <86962114+Jintonic92@users.noreply.github.com>
2026-01-08 13:34:20 +0900

    Time: 397 ms (76.80%) | Memory: 0B (100.00%) - LeetSync

--- Leetcode/177-nth-highest-salary/nth-highest-salary.sql
+++ Leetcode/177-nth-highest-salary/nth-highest-salary.sql
@@ -1,15 +1,14 @@
+
 CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
-BEGIN
-
-    SET N = N -1 ; #OFFSET에 변수 넣을 수 없음 
-  
-  RETURN (
-
-    SELECT DISTINCT SALARY 
+BEGIN 
+    SET N = N-1;
+RETURN (
+    SELECT DISTINCT salary
     FROM Employee
     ORDER BY Salary DESC
-    -- LIMIT 1 OFFSET N -1
-    LIMIT 1 OFFSET N
+    LIMIT 1 OFFSET N 
 
-  );
+
+
+);
 END
