36ad7624a5fb30f07a15e36f285986a7943a9724
Jintonic92 <86962114+Jintonic92@users.noreply.github.com>
2026-01-29 19:53:31 +0900

    [level 2] Title: 카펫, Time: 0.15 ms, Memory: 9.14 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -1,13 +1,12 @@
 def solution(brown, yellow):
     answer = []
-    
+    # w-2 * h-2 = yellow
     area = brown + yellow
     
-    # min(height) 는 min(yellow) = 1이기에 + 2해서 3임  
-    for height in range(3, int(area**0.5) + 1) : 
-        if area % height == 0 :
-            width = area // height
-            
-            # 가로와 세로에서 각각 2를 뺀 값의 곱이 yellow와 같아야 함 
-            if (width - 2) * (height - 2) == yellow :
+    # height 최소값 3
+    for height in range(3, int(area**0.5) +1):
+        if area % height == 0:
+            width = area // height 
+        
+            if (width -2 )*(height -2) == yellow:
                 return [width, height]
