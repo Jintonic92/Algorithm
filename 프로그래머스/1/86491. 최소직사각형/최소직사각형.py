db0ab86ec40deef3d4a07b8d3b4bca395cfcf381
Jintonic92 <86962114+Jintonic92@users.noreply.github.com>
2026-01-29 15:28:22 +0900

    [level 1] Title: 최소직사각형, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
@@ -1,10 +1,3 @@
 def solution(sizes):
-    answer = []
-    # 가로 세로 중 긴변 가로, 짧은변 세로에 넣기 
-    for each in sizes:
-        answer.append([max(each), min(each)])
-    
-    i = max(x[0] for x in answer)
-    j = max(x[1] for x in answer)
-    
-    return i * j
+    answer = 0
+    return answer
