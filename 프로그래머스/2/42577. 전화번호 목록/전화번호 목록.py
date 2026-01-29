5a68fec3fcb33efea043fab75b696ee65cb2e615
Jintonic92 <86962114+Jintonic92@users.noreply.github.com>
2026-01-29 13:35:47 +0900

    [level 2] Title: 전화번호 목록, Time: 92.06 ms, Memory: 29.9 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,6 +1,9 @@
 def solution(phone_book):
+    
+    # 먼저 sort 
     phone_book.sort()
     
+    # (앞, 뒤) 비교 
     for i, j in zip(phone_book, phone_book[1:]):
         if j.startswith(i):
             return False
