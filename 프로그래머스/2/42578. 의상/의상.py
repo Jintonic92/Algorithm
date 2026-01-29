9eeb52a87f803e26015761e0fa652cd678d43177
Jintonic92 <86962114+Jintonic92@users.noreply.github.com>
2026-01-29 13:43:35 +0900

    [level 2] Title: 의상, Time: 0.03 ms, Memory: 9.23 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -1,12 +1,14 @@
-from collections import Counter 
+from collections import Counter
 
 def solution(clothes):
-    types = [item[1] for item in clothes]
-    cnt = Counter(types)
+    # 경우의 수는 (종류별 개수 + 1)*
+
+    type = [x[1] for x in clothes]    
+    cnt = Counter(type)
     
     answer = 1
-    # 종류별 (개수 + 1) 곱하기 
-    for each_cnt in cnt.values() :
-        answer *= (each_cnt + 1)
+    
+    for each in cnt.values():
+        answer *= (each + 1)
     
     return answer - 1
