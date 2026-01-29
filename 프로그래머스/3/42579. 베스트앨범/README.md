26c89f24c5d70abf754054a260d4ac5fe08c2de9
Jintonic92 <86962114+Jintonic92@users.noreply.github.com>
2026-01-29 13:52:32 +0900

    [level 3] Title: 베스트앨범, Time: 0.07 ms, Memory: 9.36 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/README.md
+++ 프로그래머스/3/42579. 베스트앨범/README.md
@@ -1,10 +1,10 @@
 # [level 3] 베스트앨범 - 42579 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42579?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42579) 
 
 ### 성능 요약
 
-메모리: 9.3 MB, 시간: 0.12 ms
+메모리: 9.36 MB, 시간: 0.07 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 01월 28일 14:53:28
+2026년 01월 29일 13:52:30
 
 ### 문제 설명
 
--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -1,26 +1,22 @@
-from collections import defaultdict
+from collections import defaultdict, Counter 
 
 def solution(genres, plays):
-    
-    # 그릇 준비 
-    play_cnt = defaultdict(int)
-    genres_list = defaultdict(list)
-    
-    # 데이터 채우기
-    for i, (g, p) in enumerate(zip(genres, plays)):
-        play_cnt[g] += p
-        genres_list[g].append((p, i)) # 재생수, 인덱스 
-    
-    # 장르 order by 
-    sorted_genres = sorted(play_cnt.keys(), key = lambda x : play_cnt[x], reverse = True)
-
     answer = []
+    # 리스트별 재생 횟수 찾기 -> 순서 desc
+    # index, plays -> 순서 plays desc, index asc 
     
-    # 장르 내의 노래 선발
-    for g in sorted_genres :
-        songs = sorted(genres_list[g], key = lambda x : (-x[0], x[1])) # 재생수 DESC, 인덱스 ASC
-        answer.extend(s[1] for s in songs[:2]) # extend는 list에서 꺼내서 하나씩 넣는 것 
+    play_list = defaultdict(int)
+    each_song = defaultdict(list)
     
+    for i, (g, p) in enumerate(zip(genres, plays)):
+        play_list[g] += p
+        each_song[g].append([p, i])
+    
+    # 1. order by genre
+    play_list = sorted(play_list.keys(), key = lambda x : play_list[x], reverse = True)
+    
+    # 2. get each song 
+    for each in play_list :
+        songs = sorted(each_song[each], key = lambda x : (-x[0], x[1]))
+        answer.extend([s[1] for s in songs[:2]])
     return answer
-   
-    
