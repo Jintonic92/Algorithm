37d2c8e4e9befc3e8a343dee533636ca4ccde9c1
Jintonic92 <86962114+Jintonic92@users.noreply.github.com>
2026-01-31 21:23:21 +0900

    [Bronze I] Title: 단어 공부, Time: 80 ms, Memory: 33212 KB -BaekjoonHub

---
+++ 백준/Bronze/1157. 단어 공부/단어 공부.py
@@ -0,0 +1,14 @@
+word = input().upper()
+word_list = list(set(word))
+count_list = []
+
+for i in word_list:
+    count = word.count(i)
+    count_list.append(count)
+
+if count_list.count(max(count_list)) >1 :
+    print("?")
+
+else:
+    max_index = count_list.index(max(count_list))
+    print(word_list[max_index])
