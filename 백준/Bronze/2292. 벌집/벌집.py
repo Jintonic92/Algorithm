dd2c1b96db50074ce52bf7a3092cad87e821e4ce
Jintonic92 <86962114+Jintonic92@users.noreply.github.com>
2026-02-01 12:32:57 +0900

    [Bronze II] Title: 벌집, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2292. 벌집/벌집.py
+++ 백준/Bronze/2292. 벌집/벌집.py
@@ -1,16 +1,10 @@
-# 1 
-# +6 7  <- 6 * 1 
-# +12 19 < 6 * 2 + * 1 = 6 * 3 + 1
-# +18 37 <- 6 * 3 + *2 + * 1 = 6 * 6 + 1
-# +24 61 <- 6 * 4 + 6 * 3 + 6 * 2 + 6 * 1 = 6 * 10 + 1
-
 n = int(input())
 
 layer = 1
-current_max = 1
+max_num = 1
 
-while current_max < n:
-    current_max += layer * 6
-    layer += 1
+while max_num < n :
+  max_num += layer * 6 
+  layer += 1
 
 print(layer)
