38d07eff16b2094f7cc4682f122dadd31efa0420
Jintonic92 <86962114+Jintonic92@users.noreply.github.com>
2026-02-04 23:03:25 +0900

    [Silver V] Title: 셀프 넘버, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/4673. 셀프 넘버/셀프 넘버.py
+++ 백준/Silver/4673. 셀프 넘버/셀프 넘버.py
@@ -1,14 +1,16 @@
-dp_list = [True] * 10001
-
-for num in range(1, 10001):
-  total = num
-
-  for n in str(num):
-    total += int(n)
-
-  if total <= 10000:
-    dp_list[total] = False
+all_list = [False] * 10001
 
 for x in range(1, 10001):
-  if dp_list[x]:
+  str_x = str(x)
+  total = x
+
+  for each in str_x:
+    total += int(each)
+
+  if total < 10000:
+    all_list[total] = True
+
+for x in range(1, 10000):
+  if not all_list[x] :
     print(x)
+
