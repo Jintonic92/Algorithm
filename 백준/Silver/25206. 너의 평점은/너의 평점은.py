13573e0fa3cfd78a07e2fc599889e3a8d5f60514
Jintonic92 <86962114+Jintonic92@users.noreply.github.com>
2026-02-04 23:39:30 +0900

    [Silver V] Title: 너의 평점은, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/25206. 너의 평점은/너의 평점은.py
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.py
@@ -1,47 +1,27 @@
-# input : 수업명 학점 등급 
-# output : 평점
-# sum(학점*등급)/len(수업명)
-
-# bin = [list(row.split()) for row in """ObjectOrientedProgramming1 3.0 A+
-# IntroductiontoComputerEngineering 3.0 A+
-# ObjectOrientedProgramming2 3.0 A0
-# CreativeComputerEngineeringDesign 3.0 A+
-# AssemblyLanguage 3.0 A+
-# InternetProgramming 3.0 B0
-# ApplicationProgramminginJava 3.0 A0
-# SystemProgramming 3.0 B0
-# OperatingSystem 3.0 B0
-# WirelessCommunicationsandNetworking 3.0 C+
-# LogicCircuits 3.0 B0
-# DataStructure 4.0 A+
-# MicroprocessorApplication 3.0 B+
-# EmbeddedSoftware 3.0 C0
-# ComputerSecurity 3.0 D+
-# Database 3.0 C+
-# Algorithm 3.0 B0
-# CapstoneDesigninCSE 3.0 B+
-# CompilerDesign 3.0 D0
-# ProblemSolving 4.0 P""".split("\n")]
-bin = []
-
+# from collections import Count 
+all_list = []
+grade_num = {'A+' : 4.5
+            , 'A0' : 4.0
+            , 'B+' : 3.5
+            , 'B0' : 3.0
+            , 'C+' : 2.5 
+            , 'C0' : 2.0
+            , 'D+' : 1.5
+            , 'D0' : 1.0 
+            , 'F' : 0.0 
+             }
 for _ in range(20):
-  bin.append(input().split())
-
-grade_dict = {"A+" : 4.5, "A0" : 4.0, "B+" : 3.5, "B0" : 3.0, "C+" : 2.5, "C0" : 2.0,
-              "D+" : 1.5, "D0" : 1.0, "F" : 0.0}
-
-result = 0 
-cnt = 0 
-
-for name, num, grade in bin:
-  #print(num, grade)
-  if grade == "P":
+  course, credit, grade = input().split() 
+  if grade == 'P':
     continue
+  all_list.append([float(credit), grade_num[grade]]) # float처리 에러 방지 
 
-  result += float(num) * grade_dict[grade]
-  cnt += float(num)
+numerator = 0.0
+denominator = 0.0
 
-# print(result)
-# print(cnt)
-print(f"{result/cnt:.6f}")
+for idx in range(len(all_list)):
+  # print(all_list[idx][0] * all_list[idx][1])
+  numerator += all_list[idx][0] * all_list[idx][1]
+  denominator += all_list[idx][0]
 
+print(f"{numerator/denominator:.6f}")
