31fc38d81f64e184b252ac6688f578671602b1f0
Jintonic92 <86962114+Jintonic92@users.noreply.github.com>
2026-02-09 23:08:13 +0900

    [Silver V] Title: 방 번호, Time: 28 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1475. 방 번호/방 번호.py
+++ 백준/Silver/1475. 방 번호/방 번호.py
@@ -1,18 +1,13 @@
-word = int(input())
-
-all_list = [0] * 11
-
-word_str = str(word)
-
-for w in word_str:
-  n = int(w)
-
+num = input()
+to_ten = [0 for _ in range(10)]
+for n in num: 
+  n = int(n)
   if n == 6 or n == 9:
-    if all_list[6] < all_list[9]:
-      all_list[6] += 1
-    else: all_list[9] += 1
+    if to_ten[6] < to_ten[9]:
+      to_ten[6] += 1
+    else: to_ten[9] += 1
   
   else:
-    all_list[n] += 1
+    to_ten[n] += 1
 
-print(max(all_list))
+print(max(to_ten))
