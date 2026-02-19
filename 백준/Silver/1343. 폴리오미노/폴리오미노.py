4203aac74a33c6a2bd8919d68f6f601d5f4ec1c9
Jintonic92 <86962114+Jintonic92@users.noreply.github.com>
2026-02-19 22:45:43 +0900

    [Silver V] Title: 폴리오미노, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1343. 폴리오미노/폴리오미노.py
+++ 백준/Silver/1343. 폴리오미노/폴리오미노.py
@@ -1,10 +1,8 @@
-# all_list = input().split('.')
-# all_list = [x for x in all_list if x != '']
-all_list = input()
-all_list = all_list.replace('XXXX', 'AAAA')
-all_list = all_list.replace('XX', 'BB')
+word = input()
+word = word.replace('XXXX', 'AAAA')
+word = word.replace('XX', 'BB')
 
-if 'X' in all_list:
+if "X" in word:
   print(-1)
 else:
-  print(all_list)
+  print(word)
