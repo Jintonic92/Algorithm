52bc78c3c9967baa3154b00fc5ec3ad63872a0e5
Jintonic92 <86962114+Jintonic92@users.noreply.github.com>
2026-02-23 16:51:11 +0900

    [Silver V] Title: 2차원 배열의 합, Time: 572 ms, Memory: 38948 KB -BaekjoonHub

--- 백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.py
+++ 백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.py
@@ -1,24 +1,23 @@
-# dp[i][j] = 위 + 아래 + graph 대각선 - 대각선
-# answer[i][k] = 현재 - 위 - 아래 + 대각선 
-
 n, m = map(int, input().split())
 graph = []
+
 for _ in range(n):
   row = list(map(int, input().split()))
   graph.append(row)
 
 dp = [[0] * (m+1) for _ in range(n+1)]
 
-for i in range(1, n+1):
-  for j in range(1, m+1):
-    dp[i][j] = dp[i-1][j] + dp[i][j-1] + graph[i-1][j-1] - dp[i-1][j-1]
+# dp[i][j] = 위 + 아래 + graph 대각선 - 대각선
+# answer[i][k] = 현재 - 위 - 아래 + 대각선 
+
+for x in range(1, n+1):
+  for y in range(1, m+1):
+    dp[x][y] = dp[x-1][y] + dp[x][y-1] + graph[x-1][y-1] - dp[x-1][y-1]
 
 k = int(input())
 
 for _ in range(k):
-  i, j, k, l = map(int, input().split())
-  answer = dp[k][l] - dp[i-1][l] - dp[k][j-1] + dp[i-1][j-1]
+  a, b, c, d = map(int, input().split())
+  answer = dp[c][d] - dp[a-1][d] - dp[c][b-1] + dp[a-1][b-1]
 
   print(answer)
-
-
