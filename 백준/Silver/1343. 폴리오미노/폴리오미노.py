0ebce8cc61d4dfa87fb3874596a66e9711cf8fd8
Jintonic92 <86962114+Jintonic92@users.noreply.github.com>
2026-02-23 20:42:05 +0900

    [Silver V] Title: 폴리오미노, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1343. 폴리오미노/폴리오미노.py
+++ 백준/Silver/1343. 폴리오미노/폴리오미노.py
@@ -1,8 +1,9 @@
 word = input()
-word = word.replace('XXXX', 'AAAA')
-word = word.replace('XX', 'BB')
 
-if "X" in word:
+word = word.replace('XXXX', 'AAAA')
+word = word.replace("XX", 'BB')
+
+if 'X' in word:
   print(-1)
 else:
   print(word)
