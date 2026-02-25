f92c49a94ede35c2897847e160f2edd58bd89015
Jintonic92 <86962114+Jintonic92@users.noreply.github.com>
2026-02-25 22:10:27 +0900

    [Silver V] Title: 비밀번호 발음하기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/4659. 비밀번호 발음하기/비밀번호 발음하기.py
+++ 백준/Silver/4659. 비밀번호 발음하기/비밀번호 발음하기.py
@@ -1,47 +1,44 @@
+
 while True:
   word = input()
+  v_cnt = 0
+  c_cnt = 0
+  is_good = True
+  has_v = False
 
   if word == 'end':
     break
 
-  vowels = ['a', 'e', 'i', 'o', 'u']
-  has_vowel = False
-  v_cnt = 0
-  c_cnt = 0 
-  accept = True
-
   for i in range(len(word)):
-
-    if word[i] in vowels:
-      has_vowel = True
-      v_cnt += 1
-      c_cnt = 0 
+    
+    w = word[i]
+  
+    if w in 'aeiou':
+      has_v = True
+      c_cnt += 1
+      v_cnt = 0
     
     else:
-      v_cnt = 0
-      c_cnt += 1
-
+      c_cnt = 0
+      v_cnt += 1
     
-    if v_cnt > 2 or c_cnt > 2:
-      accept = False
+    if c_cnt > 2 or v_cnt > 2:
+      is_good = False
       break
-    
     
     if i > 0 and word[i-1] == word[i]:
       if word[i] not in 'eo':
-        accept = False
-        break 
+        is_good = False
+        break
+    
+  if has_v == False:
+    is_good = False
 
-  if has_vowel == False:
-    accept = False 
-
-  if accept == False:
+  if is_good != True:
     print(f"<{word}> is not acceptable.")
   else:
     print(f"<{word}> is acceptable.")
-
-
-      
-
-
     
+
+
+        
