7c29e8b0e362bbdc4a13c473fc69d5cd92c64bc6
Jintonic92 <86962114+Jintonic92@users.noreply.github.com>
2026-02-26 21:23:55 +0900

    [Silver V] Title: 누울 자리를 찾아라, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1652. 누울 자리를 찾아라/누울 자리를 찾아라.py
+++ 백준/Silver/1652. 누울 자리를 찾아라/누울 자리를 찾아라.py
@@ -1,22 +1,24 @@
 n = int(input())
 graph = []
-for _ in range(n):
-  graph.append(input())
 
-row = 0
-col = 0 
+for _ in range(n):
+  row = input()
+  graph.append(row)
+
+c = 0 
+r = 0
 
 for i in range(n):
-  cnt = 0 
+  cnt = 0
   for j in range(n):
     if graph[i][j] == '.':
       cnt += 1
     else:
       if cnt >= 2:
-        row += 1
-      cnt = 0
+        r += 1
+      cnt = 0 
   if cnt >= 2:
-    row += 1
+    r += 1
 
 for j in range(n):
   cnt = 0
@@ -25,9 +27,9 @@ for j in range(n):
       cnt += 1
     else:
       if cnt >= 2:
-        col += 1
-      cnt = 0
+        c += 1
+      cnt = 0 
   if cnt >= 2:
-    col +=1 
-    
-print(row, col)
+    c += 1
+
+print(r, c)
