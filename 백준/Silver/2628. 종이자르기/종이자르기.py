94e99dec21dbb32fa56f7e27e90897ce7163b463
Jintonic92 <86962114+Jintonic92@users.noreply.github.com>
2026-03-10 14:41:31 +0900

    [Silver V] Title: 종이자르기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2628. 종이자르기/종이자르기.py
+++ 백준/Silver/2628. 종이자르기/종이자르기.py
@@ -1,8 +1,8 @@
 n, m = map(int, input().split())
 k = int(input())
 
-r_list = [0, m] # 가로 list
-c_list = [0, n]  # 세로 list
+r_list = [0, m]
+c_list = [0, n]
 for _ in range(k):
   a, b = map(int, input().split())
   if a == 0:
@@ -10,26 +10,19 @@ for _ in range(k):
   else:
     c_list.append(b)
 
-r_list.sort()
-c_list.sort()
-
-# 가로 조각 중 가장 긴 길이 찾기 
-max_row = 0
-for i in range(1, len(r_list)):
-  diff = r_list[i] - r_list[i-1]
-  max_row = max(max_row, diff)
-
-# 세로 조각 중 가장 긴 길이 찾기
-max_col = 0
-for i in range(1, len(c_list)):
-  diff = c_list[i] - c_list[i-1]
-  max_col = max(max_col, diff)
-
-print(max_row * max_col)
-
-
-
+  # 조각들 중 가장 긴 길이 찾기 
+  r_list.sort()
+  c_list.sort()
+  
+  max_row = 0 
+  for i in range(1, len(r_list)):
+    diff = r_list[i] - r_list[i-1]
+    max_row = max(diff, max_row)
+  
+  max_col = 0 
+  for i in range(1, len(c_list)):
+    diff = c_list[i] - c_list[i-1]
+    max_col = max(diff, max_col)
   
 
-  
-  
+print(max_col * max_row)
