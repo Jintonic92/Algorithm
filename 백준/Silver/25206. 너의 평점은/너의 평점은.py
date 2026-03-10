7e612233da4ccbe29b37c8130271ecca9b880e8c
Jintonic92 <86962114+Jintonic92@users.noreply.github.com>
2026-03-10 13:32:13 +0900

    [Silver V] Title: 너의 평점은, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/25206. 너의 평점은/너의 평점은.py
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.py
@@ -1,27 +1,18 @@
-# from collections import Count 
-all_list = []
-grade_num = {'A+' : 4.5
-            , 'A0' : 4.0
-            , 'B+' : 3.5
-            , 'B0' : 3.0
-            , 'C+' : 2.5 
-            , 'C0' : 2.0
-            , 'D+' : 1.5
-            , 'D0' : 1.0 
-            , 'F' : 0.0 
-             }
+grade_list = {"A+" : 4.5 , "A0" : 4.0, "B+" : 3.5, "B0" : 3.0, "C+" : 2.5, "C0" : 2.0, 'D+' : 1.5, "D0" : 1.0, "F" : 0.0}
+
+a_list = []
 for _ in range(20):
-  course, credit, grade = input().split() 
+  course, credit, grade = input().split()
   if grade == 'P':
     continue
-  all_list.append([float(credit), grade_num[grade]]) # float처리 에러 방지 
+  
+  a_list.append([float(credit), float(grade_list[grade])])
 
-numerator = 0.0
-denominator = 0.0
+sum_cred = 0.0
+sum_grad = 0.0
 
-for idx in range(len(all_list)):
-  # print(all_list[idx][0] * all_list[idx][1])
-  numerator += all_list[idx][0] * all_list[idx][1]
-  denominator += all_list[idx][0]
+for cred, grad in a_list:
+  sum_grad += cred * grad
+  sum_cred += cred
 
-print(f"{numerator/denominator:.6f}")
+print(f"{sum_grad/sum_cred:.6f}")
