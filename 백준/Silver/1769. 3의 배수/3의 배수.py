fbae8b37110ce69af86bf35264e3575aa17be617
Jintonic92 <86962114+Jintonic92@users.noreply.github.com>
2026-03-11 15:00:10 +0900

    [Silver V] Title: 3의 배수, Time: 1372 ms, Memory: 43504 KB -BaekjoonHub

--- 백준/Silver/1769. 3의 배수/3의 배수.py
+++ 백준/Silver/1769. 3의 배수/3의 배수.py
@@ -1,14 +1,18 @@
-w = input()
-cnt = 0
-
-while len(w) > 1:
+x = int(input())
+cnt = 0 
+while True:
+  str_x = str(x)
   
-  total = 0
-  for i in w:
-    total += int(i)
-
-  w = str(total)
+  if len(str_x) == 1:
+    break 
+  
+  total = 0 
+  for w in str_x:
+    total += int(w)
+  
+  x = total
   cnt += 1
 
 print(cnt)
-print("YES" if int(w) % 3 == 0 else "NO")
+print("YES" if x % 3 == 0 else "NO")
+
