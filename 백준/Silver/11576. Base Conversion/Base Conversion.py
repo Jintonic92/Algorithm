06a9cf2d641cee85f7602c493a2936b75be39914
Jintonic92 <86962114+Jintonic92@users.noreply.github.com>
2026-03-12 22:35:15 +0900

    [Silver V] Title: Base Conversion, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/11576. Base Conversion/Base Conversion.py
+++ 백준/Silver/11576. Base Conversion/Base Conversion.py
@@ -1,27 +1,20 @@
-# 11576 : Base Conversion
-
-# A, B
-# A의 자리수
-# A진법의 수 
-# 결과 : B 진법으로 나타내기
-
-# 1. A진법의 수를 10진법으로 바꾸기
-# 2. 10진법으로 바뀐 수를 B진법으로 바꾸기 
-
 a, b = map(int, input().split())
-lens = int(input())
-inp_list = list(map(int, input().split()))
-ten = 0
+n = int(input())
+a_list = list(map(int, input().split()))
 
-# 1. A진법의 수를 10진법으로 바꾸기
-for idx, each in enumerate(inp_list[::-1]):
-  ten += (a ** idx) * each
 
-#print(ten)
-result = []
-while ten > 0:
-  ten, r = divmod(ten, b)
-  result.append(r)
+total = 0 
+j = 0
+for i in range(n - 1, -1, -1):
+    total += a_list[i] * a**j
+    j += 1
 
-print(*result[::-1])
+if total == 0:
+  print(0)
+else:
+  answer = []
+  while total > 0:
+    answer.append(total % b)
+    total //= b
 
+  print(*(answer[::-1]))
