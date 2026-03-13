428e349711234276d9b396bd50824bc575a828e4
Jintonic92 <86962114+Jintonic92@users.noreply.github.com>
2026-03-13 14:04:39 +0900

    [Silver V] Title: 생일, Time: 36 ms, Memory: 34592 KB -BaekjoonHub

--- 백준/Silver/5635. 생일/생일.py
+++ 백준/Silver/5635. 생일/생일.py
@@ -1,14 +1,12 @@
 from datetime import date
-
 n = int(input())
 a_list = []
+
 for _ in range(n):
   name, d, m, y = input().split()
   today = date.today()
-  birth = date(int(y), int(m), int(d))
-  a_list.append([name, (today-birth).days])
+  a_list.append([name, (today - date(int(y), int(m), int(d))).days])
 
 a_list.sort(key = lambda x : x[1])
-
 print(a_list[0][0])
 print(a_list[-1][0])
