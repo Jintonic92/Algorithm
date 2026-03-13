851e57b02abfa2cfff29561061e475e1eb9db664
Jintonic92 <86962114+Jintonic92@users.noreply.github.com>
2026-03-13 14:13:46 +0900

    [Silver V] Title: 국회의원 선거, Time: 40 ms, Memory: 35640 KB -BaekjoonHub

--- 백준/Silver/1417. 국회의원 선거/국회의원 선거.py
+++ 백준/Silver/1417. 국회의원 선거/국회의원 선거.py
@@ -1,25 +1,28 @@
 import heapq
+
 n = int(input())
 goal = int(input())
-
 a_list = []
-
 for _ in range(n-1):
-  n = int(input())
-  heapq.heappush(a_list, -n)
+  k = int(input())
+  heapq.heappush(a_list, -k)
 
-cnt = 0 
-while a_list:
-  x = -heapq.heappop(a_list)
+cnt = 0
+if len(a_list) == 0:
+  print(cnt)
+else: 
+  while True :
+
+    x = -heapq.heappop(a_list)
+
+    if goal > x:
+      break 
+    
+    cnt += 1
+    x -= 1
+    goal += 1
+
+    heapq.heappush(a_list, -x)
+
+  print(cnt)
   
-  if goal > x:
-    break 
-
-  x -= 1
-  goal += 1
-  cnt +=1 
-
-  heapq.heappush(a_list, -x)
-  
-
-print(cnt)
