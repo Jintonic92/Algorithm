8d2e977943769c499ddd3a473f5c55e4c12e88f9
Jintonic92 <86962114+Jintonic92@users.noreply.github.com>
2026-03-18 15:15:06 +0900

    [Silver V] Title: 임시 반장 정하기, Time: 800 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1268. 임시 반장 정하기/임시 반장 정하기.py
+++ 백준/Silver/1268. 임시 반장 정하기/임시 반장 정하기.py
@@ -1,27 +1,24 @@
 n = int(input())
-
 graph = []
 for _ in range(n):
   line = list(map(int, input().split()))
   graph.append(line)
 
-
-# 한명씩 다른애들이랑 비교 
-# 한번이라도 같은반 했다 ? => cnt += 1
-pred = 0 # 임시임시반장
+pred = 0 
 max_cnt = -1
 for i in range(n):
   cnt = 0 
   for j in range(n):
-    if i == j : # 동일인물 패스
+    if i == j : # 같은 아이라면
       continue 
-    for k in range(5):
+    
+    for k in range(5): #5학년동안 
       if graph[i][k] == graph[j][k]:
         cnt += 1
-        break  # 한번이라도 같은반하면 쿨패스 
-  
-  if cnt > max_cnt:
+        break 
+
+  if cnt > max_cnt :
     max_cnt = cnt
-    pred = i + 1
+    pred = i + 1 
 
 print(pred)
