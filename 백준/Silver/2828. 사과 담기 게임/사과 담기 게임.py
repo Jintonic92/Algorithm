fd80d1b0a3ed9145488f406d68b3bdac8b6b58ac
Jintonic92 <86962114+Jintonic92@users.noreply.github.com>
2026-03-18 14:49:56 +0900

    [Silver V] Title: 사과 담기 게임, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2828. 사과 담기 게임/사과 담기 게임.py
+++ 백준/Silver/2828. 사과 담기 게임/사과 담기 게임.py
@@ -1,28 +1,25 @@
 n, m = map(int, input().split())
-left = 1
-right = m # 바 오른쪽
-
 j = int(input())
+left = 1
+right = m 
 
-total_move = 0
-
+total_move = 0 
 for _ in range(j):
   apple = int(input())
-
-  if apple < left : 
+  
+  if apple < left:
     move = left - apple
     total_move += move
-    left = apple 
+    left = apple
     right -= move 
   
-  elif apple > right :
-    move = apple - right 
+  elif apple > right:
+    move = apple - right
     total_move += move
-    right = apple 
-    left += move 
+    left += move
+    right = apple
   
   else:
     continue
-    
+
 print(total_move)
-    
