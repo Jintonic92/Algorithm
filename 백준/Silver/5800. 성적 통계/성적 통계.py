8482bd1379d1f24d3e695a517e1b7198fa0af6d1
Jintonic92 <86962114+Jintonic92@users.noreply.github.com>
2026-03-18 14:53:39 +0900

    [Silver V] Title: 성적 통계, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/5800. 성적 통계/성적 통계.py
+++ 백준/Silver/5800. 성적 통계/성적 통계.py
@@ -2,16 +2,16 @@ n = int(input())
 
 for idx in range(1, n+1):
   line = list(map(int, input().split()))
-
   m = line[0]
-  data = line[1:]
+  line = line[1:]
 
-  data.sort()
-  max_diff = 0
-  max_n = max(data)
-  min_n = min(data)
+  max_score = max(line) 
+  min_score = min(line)
+  
+  line.sort()
+  max_diff = 0 
   for i in range(1, m):
-    diff = data[i] - data[i-1]
+    diff = line[i] - line[i-1]
     max_diff = max(diff, max_diff)
   print(f"Class {idx}")
-  print(f"Max {max_n}, Min {min_n}, Largest gap {max_diff}")
+  print(f"Max {max_score}, Min {min_score}, Largest gap {max_diff}")
