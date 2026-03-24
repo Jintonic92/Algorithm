8bd5714dc87c749573945b9001ca7e38e32b17e5
Jintonic92 <86962114+Jintonic92@users.noreply.github.com>
2026-03-24 22:55:52 +0900

    [Silver V] Title: 행렬 곱셈, Time: 188 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/2740. 행렬 곱셈/행렬 곱셈.py
+++ 백준/Silver/2740. 행렬 곱셈/행렬 곱셈.py
@@ -1,20 +1,22 @@
 n, m = map(int, input().split())
 a = []
+
 for _ in range(n):
   line = list(map(int, input().split()))
   a.append(line)
 
 m, k = map(int, input().split())
 b = []
+
 for _ in range(m):
   line = list(map(int, input().split()))
   b.append(line)
 
-a_list = [[0] * k for _ in range(n)]
+answer = [[0] * k for _ in range(n)]
 for x in range(n):
   for y in range(k):
-    for z in range(m): # 공통
-      a_list[x][y] += a[x][z] * b[z][y]
+    for z in range(m):
+      answer[x][y] += a[x][z] * b[z][y]
 
-for row in a_list:
+for row in answer:
   print(*row)
