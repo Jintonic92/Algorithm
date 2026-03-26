fb326189605bbaf0729a038b2cbbda9bc068ca85
Jintonic92 <86962114+Jintonic92@users.noreply.github.com>
2026-03-26 12:13:07 +0900

    [Silver V] Title: 임시 반장 정하기, Time: 948 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1268. 임시 반장 정하기/임시 반장 정하기.py
+++ 백준/Silver/1268. 임시 반장 정하기/임시 반장 정하기.py
@@ -1,26 +1,23 @@
 n = int(input())
-
-a_list = []
+graph = []
 for _ in range(n):
   line = list(map(int, input().split()))
-  a_list.append(line)
+  graph.append(line)
 
-pred = 0
 max_cnt = -1 
+temp = 0
 for i in range(n):
   cnt = 0 
   for j in range(n):
     if i == j :
       continue
     for k in range(5):
-      if a_list[i][k] == a_list[j][k]:
+      if graph[i][k] == graph[j][k]:
         cnt += 1
         break 
-  
-  if cnt > max_cnt :
-    max_cnt = cnt
-    pred = i +1 
+    
+    if cnt > max_cnt :
+      max_cnt = cnt 
+      temp = i + 1
 
-print(pred)
-  
-  
+print(temp)
