17f08f691e0b39dbd10e717380dcf7a610d0c9c9
Jintonic92 <86962114+Jintonic92@users.noreply.github.com>
2026-03-26 11:51:48 +0900

    [Silver V] Title: 누울 자리를 찾아라, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1652. 누울 자리를 찾아라/누울 자리를 찾아라.py
+++ 백준/Silver/1652. 누울 자리를 찾아라/누울 자리를 찾아라.py
@@ -1,39 +1,35 @@
 n = int(input())
-
 graph = []
 for _ in range(n):
   line = input()
   graph.append(line)
 
 row_cnt = 0
-for i in range(n):
-  cnt = 0
-  for j in range(n):
-    if graph[i][j] == '.':
-      cnt += 1
 
-    else:
-      
-      if cnt >= 2:
+for x in range(n):
+  cnt = 0
+  for y in range(n):
+    if graph[x][y] == '.':
+      cnt += 1
+    else:    
+      if cnt >= 2 :
         row_cnt += 1
-      cnt = 0 
-    
-    if j == n-1 and cnt >= 2:
-      row_cnt += 1
+      cnt = 0
+    if y == n -1 and cnt >= 2:
+      row_cnt +=1 
+
 
 col_cnt = 0
-for j in range(n):
+for y in range(n):
   cnt = 0
-  for i in range(n):
-    if graph[i][j] == '.':
+  for x in range(n):
+    if graph[x][y] == '.':
       cnt += 1
-
-    else:
-      if cnt >= 2:
+    else:    
+      if cnt >= 2 :
         col_cnt += 1
       cnt = 0
     
-    if i == n-1 and cnt >= 2:
-      col_cnt += 1
-
-print(row_cnt, col_cnt)  
+    if x == n -1 and cnt >= 2:
+      col_cnt +=1 
+print(row_cnt, col_cnt)
