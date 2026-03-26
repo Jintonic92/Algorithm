3d65d1e756f4e05a61c25de981cd08278d3b3a08
Jintonic92 <86962114+Jintonic92@users.noreply.github.com>
2026-03-26 12:57:05 +0900

    [Silver V] Title: D-Day, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1308. D－Day/D－Day.py
@@ -0,0 +1,31 @@
+def is_leap(y):
+  return (y % 4 == 0 and y % 100 != 0) or (y % 400 == 0 )
+
+def get_days(y, m, d):
+  months = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+  # 연도별 일수 
+  total = 0
+  for i in range(1, y):
+    total += 366 if is_leap(i) else 365
+  
+  # 월별 일수
+  for i in range(1, m):
+    if i == 2 and is_leap(y):
+      total += 29 
+    else:
+      total += months[i]
+  
+  # 일수
+  return total + d
+
+y1, m1, d1 = map(int, input().split())
+y2, m2, d2 = map(int, input().split())
+
+if y2 - y1 > 1000:
+    print("gg")
+elif y2 - y1 == 1000 and (m2 > m1 or (m2 == m1 and d2 >= d1)):
+    print("gg")
+else:
+    diff = get_days(y2, m2, d2) - get_days(y1, m1, d1)
+    print(f"D-{diff}")
