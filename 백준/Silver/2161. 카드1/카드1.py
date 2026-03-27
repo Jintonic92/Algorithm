0e6e239c27c19be4e3d9cac535ecd49d74613576
Jintonic92 <86962114+Jintonic92@users.noreply.github.com>
2026-03-27 12:04:38 +0900

    [Silver V] Title: 카드1, Time: 64 ms, Memory: 35040 KB -BaekjoonHub

--- 백준/Silver/2161. 카드1/카드1.py
+++ 백준/Silver/2161. 카드1/카드1.py
@@ -1,12 +1,12 @@
 from collections import deque
 
 n = int(input())
-a_list = deque(x for x in range(1, n+1))
+a_list = [x for x in range(1, n+1)]
+a_list = deque(a_list)
 answer = []
-
 while len(a_list) > 1:
-  answer.append(a_list.popleft())
+  x = a_list.popleft()
+  answer.append(x)
   a_list.append(a_list.popleft())
 
-print(*answer, a_list[0])
-  
+print(*answer, *a_list)
