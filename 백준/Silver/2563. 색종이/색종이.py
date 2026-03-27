330becaee0466e0d8f04216cc805fce513d1b2de
Jintonic92 <86962114+Jintonic92@users.noreply.github.com>
2026-03-27 11:39:14 +0900

    [Silver V] Title: 색종이, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.py
+++ 백준/Silver/2563. 색종이/색종이.py
@@ -1,10 +1,16 @@
-graph = [[False] * 100 for _ in range(100)]
+a_list = [[0] * 101 for _ in range(101)]
 
 n = int(input())
 for _ in range(n):
   x, y = map(int, input().split())
   for i in range(x, x+10):
     for j in range(y, y+10):
-      graph[i][j] = True
+      a_list[i][j] = 1
 
-print(sum(sum(row) for row in graph))
+cnt = 0 
+for i in range(101):
+  for j in range(101):
+    if a_list[i][j] == 1:
+      cnt += 1
+
+print(cnt)
