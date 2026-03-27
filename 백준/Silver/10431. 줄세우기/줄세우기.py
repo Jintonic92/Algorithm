dd015ada9f81b76296a02dc0b43fab7327667e08
Jintonic92 <86962114+Jintonic92@users.noreply.github.com>
2026-03-27 13:42:14 +0900

    [Silver V] Title: 줄세우기, Time: 116 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/10431. 줄세우기/줄세우기.py
+++ 백준/Silver/10431. 줄세우기/줄세우기.py
@@ -1,16 +1,14 @@
 n = int(input())
-a_list = []
-for idx in range(1, n+1):
-  line = list(map(int, input().split()))
-  a_list.append(line)
 
-  m = line[0]
+for _ in range(n):
+  line = list(map(int, input().split()))
+  idx = line[0]
   line = line[1:]
   cnt = 0
 
-  for k in range(len(line)):
-    for j in range(k+1, len(line)):
-      if line[k] > line[j]:
+  for i in range(len(line)):
+    for j in range(i+1, len(line)):
+      if line[i] > line[j]:
         cnt += 1
-  
+    
   print(idx, cnt)
