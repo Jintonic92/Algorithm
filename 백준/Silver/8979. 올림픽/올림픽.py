2f608352760adc0dd237aafb8e2fae10b0330320
Jintonic92 <86962114+Jintonic92@users.noreply.github.com>
2026-03-27 12:14:47 +0900

    [Silver V] Title: 올림픽, Time: 284 ms, Memory: 32412 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.py
+++ 백준/Silver/8979. 올림픽/올림픽.py
@@ -1,21 +1,25 @@
+n_list = [0] * 10001
 
-n, k = map(int, input().split())
+n, m = map(int, input().split())
+
 a_list = []
-
 for _ in range(n):
   line = list(map(int, input().split()))
   a_list.append(line)
 
-r_list = [0] * 1001
 for nat, a, b, c in a_list:
   rank = 1
   for nat_c, a_c, b_c, c_c in a_list:
     if a < a_c :
       rank += 1
-    if a == a_c and b < b_c :
-      rank += 1
-    if a == a_c and b == b_c and c < c_c:
+    
+    elif a == a_c and b < b_c :
       rank += 1
     
-    r_list[nat] = rank 
-print(r_list[k])
+    elif a ==  a_c and b == b_c and c < c_c:
+      rank += 1
+  
+    n_list[nat] = rank
+
+print(n_list[m])
+
