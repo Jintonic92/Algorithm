e7c8863e5b09c114e9417f4d848101baaf58cf94
Jintonic92 <86962114+Jintonic92@users.noreply.github.com>
2026-03-27 13:46:56 +0900

    [Silver V] Title: 아시아 정보올림피아드, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2535. 아시아 정보올림피아드/아시아 정보올림피아드.py
+++ 백준/Silver/2535. 아시아 정보올림피아드/아시아 정보올림피아드.py
@@ -1,22 +1,22 @@
-
 n = int(input())
+
 a_list = []
-answer = []
 for _ in range(n):
   line = list(map(int, input().split()))
   a_list.append(line)
 
 a_list.sort(key = lambda x : x[2], reverse = True)
-n_list = [0] * 1001
 
+dp = [0] * 101
+
+answer = []
 for i in range(n):
-  nat, idx, score = a_list[i]
+  nat, idx, s = a_list[i]
+
+  if dp[nat] < 2:
+    dp[nat] += 1
+
+    answer.append([nat, idx])
   
-  if n_list[nat] >= 2:
-    continue 
-
-  n_list[nat] += 1
-  answer.append([nat, idx]) 
-
 for row in answer[:3]:
   print(*row)
