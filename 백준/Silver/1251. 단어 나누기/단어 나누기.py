eb66a911ecde0641a48a6fbc66b66d12676d9873
Jintonic92 <86962114+Jintonic92@users.noreply.github.com>
2026-03-27 12:37:57 +0900

    [Silver V] Title: 단어 나누기, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1251. 단어 나누기/단어 나누기.py
+++ 백준/Silver/1251. 단어 나누기/단어 나누기.py
@@ -1,11 +1,11 @@
-word = input()
+w = input()
 
 a_list = []
-for i in range(1, len(word)-1):
-  for j in range(i+1, len(word)):
-    a, b, c = word[:i], word[i:j], word[j:]
-    a, b, c = a[::-1], b[::-1], c[::-1]
+for i in range(1, len(w)-1):
+  for j in range(i+1, len(w)):
+      a, b, c = w[:i], w[i:j], w[j:]
+      a, b, c = a[::-1], b[::-1], c[::-1]
 
-    a_list.append(a+b+c)
+      a_list.append(a+b+c)
 
 print(min(a_list))
