273441ebd3c898223749d52e308bf384e577a70c
Jintonic92 <86962114+Jintonic92@users.noreply.github.com>
2026-03-27 13:35:07 +0900

    [Silver V] Title: 기상캐스터, Time: 80 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/10709. 기상캐스터/기상캐스터.py
+++ 백준/Silver/10709. 기상캐스터/기상캐스터.py
@@ -1,18 +1,19 @@
 n, m = map(int, input().split())
-a_list = []
+graph = []
+
 for _ in range(n):
   line = input()
-  a_list.append(line)
+  graph.append(line)
 
-graph = [[-1] * m for _ in range(n)]
+a_list = [[-1] * m for _ in range(n)]
 
 for i in range(n):
   for j in range(m):
-    if a_list[i][j] == 'c':
-      graph[i][j] = 0 
+    if graph[i][j] == 'c':
+      a_list[i][j] = 0
       for k in range(j, m):
-        if a_list[i][k] == '.':
-          graph[i][k] = graph[i][k-1] + 1
-    
-for row in graph:
+        if graph[i][k] == '.':
+          a_list[i][k] = a_list[i][k-1] + 1
+
+for row in a_list:
   print(*row)
