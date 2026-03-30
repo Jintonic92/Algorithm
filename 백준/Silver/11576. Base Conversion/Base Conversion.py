b59cc76ef9e0dab3a4ad8123d571e6795abd6e90
Jintonic92 <86962114+Jintonic92@users.noreply.github.com>
2026-03-30 22:46:21 +0900

    [Silver V] Title: Base Conversion, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/11576. Base Conversion/Base Conversion.py
+++ 백준/Silver/11576. Base Conversion/Base Conversion.py
@@ -1,15 +1,15 @@
-n, m = map(int, input().split())
+a, b = map(int, input().split())
 k = int(input())
 line = list(map(int, input().split()))
-x = 0 
 
+total = 0
 for i in range(k):
-  x += line[k-i-1] * n ** i
+  total += line[::-1][i] * a ** i
 
-a_list = []
-while x > 0 :
-  y = x % m 
-  x //= m 
-  a_list.append(y)
+answer = []
+while total > 0 :
+  x = total % b
+  total //= b
+  answer.append(x)
 
-print(*a_list[::-1])
+print(*answer[::-1])
