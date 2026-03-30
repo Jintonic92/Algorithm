ff35db26c567dd29eb862e378454987accecf15b
Jintonic92 <86962114+Jintonic92@users.noreply.github.com>
2026-03-30 22:28:57 +0900

    [Silver V] Title: 누울 자리를 찾아라, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1652. 누울 자리를 찾아라/누울 자리를 찾아라.py
+++ 백준/Silver/1652. 누울 자리를 찾아라/누울 자리를 찾아라.py
@@ -1,35 +1,34 @@
 n = int(input())
-graph = []
+a_list = []
 for _ in range(n):
   line = input()
-  graph.append(line)
+  a_list.append(line)
 
-row_cnt = 0
-
-for x in range(n):
+row_cnt = 0 
+for i in range(n):
   cnt = 0
-  for y in range(n):
-    if graph[x][y] == '.':
+  for j in range(n):
+    if a_list[i][j] == '.':
       cnt += 1
-    else:    
-      if cnt >= 2 :
+    else:
+      if cnt >= 2:
         row_cnt += 1
       cnt = 0
-    if y == n -1 and cnt >= 2:
-      row_cnt +=1 
+    if j == n -1 and cnt >= 2:
+      row_cnt += 1
 
-
-col_cnt = 0
-for y in range(n):
-  cnt = 0
-  for x in range(n):
-    if graph[x][y] == '.':
+col_cnt = 0 
+for j in range(n):
+  cnt = 0 
+  for i in range(n):
+    if a_list[i][j] == '.':
       cnt += 1
-    else:    
-      if cnt >= 2 :
+    else:
+      if cnt >= 2:
         col_cnt += 1
       cnt = 0
-    
-    if x == n -1 and cnt >= 2:
-      col_cnt +=1 
+    if i == n -1 and cnt >= 2:
+      col_cnt += 1
+
+
 print(row_cnt, col_cnt)
