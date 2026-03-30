1c883681a3919be560c0a4447fea3cc4524c0aab
Jintonic92 <86962114+Jintonic92@users.noreply.github.com>
2026-03-30 23:18:36 +0900

    [Silver V] Title: 🐜 기적의 매매법 🐜, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/20546. 🐜 기적의 매매법 🐜/🐜 기적의 매매법 🐜.py
+++ 백준/Silver/20546. 🐜 기적의 매매법 🐜/🐜 기적의 매매법 🐜.py
@@ -1,33 +1,41 @@
 seed = int(input())
 line = list(map(int, input().split()))
 
-# 준현
-jh_curr = seed
-max_b = 0  
-for i in range(len(line)):
-  if jh_curr >= line[i]:
-    max_b += jh_curr // line[i]
-    jh_curr %= line[i]
-  
-  if i == len(line)-1:
-    jh_total = jh_curr + line[i] * max_b
+# bnp 
+bnp = seed
+cnt = 0
+total_bnp = 0
 
-# 성민
-sh_curr = seed
-max_b = 0
+for i in range(len(line)):
+  if bnp >= line[i]:
+    cnt += bnp // line[i]
+    bnp %= line[i]
+  if i == len(line) -1 :
+    total_bnp = bnp + line[i] * cnt
+  # print(bnp, max_buy, line[i])
+
+timing = seed
+cnt = 0
+total_timing = 0 
 for i in range(3, len(line)):
   if line[i-3] > line[i-2] > line[i-1] > line[i]:
-    if sh_curr >= line[i]:
-      max_b += sh_curr // line[i]
-      sh_curr %= line[i]
+    if timing >= line[i]:
+      cnt += timing // line[i]
+      timing %= line[i]
   
   elif line[i-3] < line[i-2] < line[i-1] < line[i]:
-    sh_curr += max_b * line[i]
-    max_b = 0
-  
-  if i == len(line)-1:
-    sh_total = sh_curr + line[i] * max_b
+    if timing > 0:
+      timing += cnt * line[i]
+      cnt = 0
 
-if jh_total > sh_total : print("BNP")
-elif jh_total < sh_total : print("TIMING")
-else: print("SAMESAME")
+  if i == len(line) - 1:
+    total_timing = timing + line[i] * cnt 
+
+if total_timing > total_bnp :
+  print("TIMING")
+
+elif total_timing < total_bnp :
+  print("BNP")
+
+else:
+  print("SAMESAME")
