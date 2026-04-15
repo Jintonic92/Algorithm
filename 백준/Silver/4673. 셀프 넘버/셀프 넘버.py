0eac10dfeb49d8e10444b150372546c7d630be2f
Jintonic92 <86962114+Jintonic92@users.noreply.github.com>
2026-04-15 14:31:10 +0900

    [Silver V] Title: 셀프 넘버, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/4673. 셀프 넘버/셀프 넘버.py
+++ 백준/Silver/4673. 셀프 넘버/셀프 넘버.py
@@ -1,16 +1,17 @@
-all_list = [False] * 10001
+visited = [0] * 10001
 
-for x in range(1, 10001):
-  str_x = str(x)
-  total = x
+def make_lone(str_x):
+  total = int(str_x)
+  for i in str_x:
+    total += int(i) 
+  return total
 
-  for each in str_x:
-    total += int(each)
+for i in range(10001):
+  # print(i)
+  x = make_lone(str(i))
+  if x <= 10000:
+    visited[x] = True
 
-  if total < 10000:
-    all_list[total] = True
-
-for x in range(1, 10000):
-  if not all_list[x] :
-    print(x)
-
+for j in range(10001):
+  if not visited[j] :
+    print(j)
