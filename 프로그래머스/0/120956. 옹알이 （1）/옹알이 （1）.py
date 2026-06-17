d074fde8f050efba7671947e66f7884e3be06904
Jintonic92 <86962114+Jintonic92@users.noreply.github.com>
2026-06-17 12:04:29 +0900

    [level 0] Title: 옹알이 (1), Time: 0.03 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.py
+++ 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.py
@@ -1,13 +1,12 @@
 a_list = ['aya', 'ye', 'woo', 'ma']
 def solution(babbling):
-    answer = 0 
-    
+    answer = 0
     for b in babbling:
         for a in a_list:
-            b = b.replace(a, " ")
-
-        # print(b, b.strip())
-        if b.strip() == "":
+            b = b.replace(a, ' ')
+        
+        if b.strip() == '':
             answer += 1
-    
+            
+            
     return answer
