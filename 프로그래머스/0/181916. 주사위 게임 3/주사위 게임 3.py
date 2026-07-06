79746bc741880dae0515616dfef415394f7bbaab
Jintonic92 <86962114+Jintonic92@users.noreply.github.com>
2026-07-06 16:33:43 +0900

    [level 0] Title: 주사위 게임 3, Time: 0.01 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.py
+++ 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.py
@@ -1,30 +1,34 @@
-visited = [0] * 7
-
 def solution(a, b, c, d):
     answer = 0
+    # 각 주사위 수를 셈 
+    visited = [0] * 7
     visited[a] += 1
     visited[b] += 1
     visited[c] += 1
     visited[d] += 1
     
+    # 4개가 같으면 max(visited) == 4
+    # 3개가 같고 하나만 다르다면 max(visited) == 3
+    # 2개가 같고 
+        # 나머지도 같으면 len(set()) == 2
+        # 나머지가 다르다면 len(set()) == 3
     if max(visited) == 4:
         return 1111 * a
-    
     if max(visited) == 3:
         idx3 = [i for i, value in enumerate(visited) if value == 3][0]
         idx1 = [i for i, value in enumerate(visited) if value == 1][0]
         return (10 * idx3 + idx1)**2
-    
-    if visited.count(2) == 2:
-        idx1 = [i for i, value in enumerate(visited) if value == 2][0]
-        idx2 = [i for i, value in enumerate(visited) if value == 2][1]
-        return (idx1 + idx2) * abs(idx1 - idx2)
-    
-    if max(visited) == 2:
-        idx1 = [i for i, value in enumerate(visited) if value == 1][0]
-        idx2 = [i for i, value in enumerate(visited) if value == 1][1]
-        return idx1 * idx2
-    
-    else :
+    if len(set([a, b, c, d])) == 2:
+        idx2 = [i for i, value in enumerate(visited) if value == 2][0]
+        idx22 = [i for i, value in enumerate(visited) if value == 2][1]
+        return (idx2 + idx22) * abs(idx2 - idx22)
+    if len(set([a, b, c, d])) == 3:
+        idx2 = [i for i, value in enumerate(visited) if value == 1][0]
+        idx3 = [i for i, value in enumerate(visited) if value == 1][1]
+        return idx2 * idx3
+    else:
         return min(a, b, c, d)
-    
+       
+
+    return answer
+
