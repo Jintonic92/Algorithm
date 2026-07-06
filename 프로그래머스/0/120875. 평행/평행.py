80d28148e848d1ececfe6346dbc9715b5cb0e4d8
Jintonic92 <86962114+Jintonic92@users.noreply.github.com>
2026-07-06 15:58:28 +0900

    [level 0] Title: 평행, Time: 0.01 ms, Memory: 11.2 MB -BaekjoonHub

--- 프로그래머스/0/120875. 평행/평행.py
+++ 프로그래머스/0/120875. 평행/평행.py
@@ -1,18 +1,15 @@
 def solution(dots):
     answer = 0
     
-    # 평행이라는 것은 
-    # 두 선의 기울이가 같다는 것 
-    # 기울기 = diff.x / diff.y
-    # 점 4개에서 만들 수 있는 3쌍 
+    # 기울기가 같다 == diff.x /diff.y 가 같다 
+    # 점이 4개인 경우 한쌍의 선이 나올 수 있는 경우 3가지 
+    
     a, b, c, d = dots[0], dots[1], dots[2], dots[3]
     
-    if (a[0] - c[0]) / (b[0] - d[0]) == (a[1] - c[1]) / (b[1] - d[1]):
-        answer = 1
-    
-    if (a[0] - b[0]) / (c[0] - d[0]) == (a[1] - b[1]) / (c[1] - d[1]):
-        answer = 1
-    
-    if (a[0] - d[0]) / (b[0] - c[0]) == (a[1] - d[1]) / (b[1] - d[1]):
-        answer = 1
+    if (a[0] - b[0]) / (a[1] - b[1]) == (c[0] - d[0]) / (c[1] - d[1]):
+        return 1
+    if (a[0] - c[0]) / (a[1] - c[1]) == (b[0] - d[0]) / (b[1] - d[1]):
+        return 1
+    if (a[0] - d[0]) / (a[1] - d[1]) == (b[0] - c[0]) / (b[1] - c[1]):
+        return 1
     return answer
