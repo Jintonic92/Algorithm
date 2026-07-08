ffee4f488ba2a8f4ddde634ff1ffe7730ba32d11
Jintonic92 <86962114+Jintonic92@users.noreply.github.com>
2026-07-08 15:31:43 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.01 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.py
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.py
@@ -1,25 +1,20 @@
 def gcd(a, b):
     a, b = max(a, b), min(a, b)
-    while b > 0 :
-        a, b = b, a%b
+    while b > 0:
+        a, b = b, a%b 
     return a
 
 def solution(numer1, denom1, numer2, denom2):
     answer = []
-    # 분수의 덧셈
-    # a/b + c/d = ad + bc / bd 
-    # 기약함수로 나타내는 방법 : 분자/모를 최대공약수로 나눔 
     
-    numer = numer1 * denom2 + numer2 * denom1
-    denom = denom1 * denom2
-    
-    gcd_num = gcd(numer, denom)
-    
-    numer /= gcd_num 
-    denom /= gcd_num 
-    
-    answer.append(numer)
-    answer.append(denom)
-    
+    # 기약수 = 최대 공배수로 나눈 값
+    n = numer1 * denom2 + numer2 * denom1 
+    d = denom1 * denom2 
+    # print(n, d, gcd(n, d), d//gcd(n, d))
+    num = n // gcd(n, d)
+    den = d // gcd(n, d)
+ 
+    answer.append(num)
+    answer.append(den)
     
     return answer
