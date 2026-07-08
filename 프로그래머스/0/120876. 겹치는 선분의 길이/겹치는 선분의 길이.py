403384011ab891c90c23b14524eb1b8ffe63b0e5
Jintonic92 <86962114+Jintonic92@users.noreply.github.com>
2026-07-08 15:26:53 +0900

    [level 0] Title: 겹치는 선분의 길이, Time: 0.02 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.py
+++ 프로그래머스/0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.py
@@ -1,17 +1,15 @@
 def solution(lines):
     answer = 0
-    # 겹치는 길이 
-    # visited으로 겹치는 구간 표시 
-    # visited >= 2 인 애들의 길이 표시
-    
+    # visited으로 겹치는 선 관리 : > 2이면 내뱉기 
     visited = [0] * 201
     
     for s, e in lines:
-        s, e = s + 100, e + 100
+        s += 100
+        e += 100 
         for i in range(s, e):
             visited[i] += 1
     
-    for x in range(200):
+    for x in range(201):
         if visited[x] >= 2:
             answer += 1
     return answer
