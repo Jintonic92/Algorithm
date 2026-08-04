bee3bc62f7d5569ed8540f1dd45a30f0ae39342b
Jintonic92 <86962114+Jintonic92@users.noreply.github.com>
2026-08-04 14:18:38 +0900

    [level 0] Title: 정수를 나선형으로 배치하기, Time: 0.11 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/0/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.py
+++ 프로그래머스/0/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.py
@@ -1,19 +1,30 @@
 def solution(n):
     answer = [[0] * n for _ in range(n)]
     
+    # 0, 0 부터 시작해서 
+    # 오른쪽 > 아래 > 왼쪽 > 위 순으로 돌기
+    # 방향 트는 기준 : nx, ny 가 n을 넘는다면 
+    # 방향 트는 방법 : indexing % 4 
+    # 돌면서 +1 씩 남기고
+    
     x, y = 0, 0
-    idx = 0 
     dx = [0, 1, 0, -1]
     dy = [1, 0, -1, 0]
+    idx = 0 
     
-    for i in range(1, n**2+1):
+    for i in range(1, n**2 + 1):
         answer[x][y] = i
         
-        nx, ny = x + dx[idx], y + dy[idx]
+        nx = x + dx[idx]
+        ny = y + dy[idx]
         
-        if nx >= n or ny >= n or nx < 0 or ny < 0 or answer[nx][ny] != 0:
-            idx = (idx+1) % 4
-            nx, ny = x + dx[idx], y + dy[idx]
-        
-        x, y = nx, ny 
+        if nx >= n or ny >= n or answer[nx][ny] != 0 :
+            idx = (idx + 1) % 4
+            nx = x + dx[idx]
+            ny = y + dy[idx]
+            
+        x, y = nx, ny
+    
+    
     return answer
+
