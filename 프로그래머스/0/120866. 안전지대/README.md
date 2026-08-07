1ee08507b080d451c5a84cb5c6d3c44e13d082ff
Jintonic92 <86962114+Jintonic92@users.noreply.github.com>
2026-08-07 14:21:38 +0900

    [level 0] Title: 안전지대, Time: 0.04 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/0/120866. 안전지대/README.md
+++ 프로그래머스/0/120866. 안전지대/README.md
@@ -1,10 +1,10 @@
 # [level 0] 안전지대 - 120866 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120866?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120866) 
 
 ### 성능 요약
 
-메모리: 11.6 MB, 시간: 0.04 ms
+메모리: 11.3 MB, 시간: 0.04 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 08월 04일 15:18:07
+2026년 08월 07일 14:21:35
 
 ### 문제 설명
 
--- 프로그래머스/0/120866. 안전지대/안전지대.py
+++ 프로그래머스/0/120866. 안전지대/안전지대.py
@@ -1,24 +1,26 @@
 def solution(board):
     answer = 0
-    # 사방 확인시 1이면 지나가기
-    dx = [-1, -1, -1, 0, 0, 1, 1, 1]
-    dy = [-1, 0, 1, -1, 1, -1, 0, 1]
+    # 지뢰를 먼저 찾아서 주위를 다 1로 만들기 
+    # 그리고 전체 돌면서 0인 지역 찾기 
+    n = len(board)
+    visited = [[0] * n for _ in range(n)]
     
-    visited = [[0] * len(board) for _ in range(len(board))]
-    for x in range(len(board)):
-        for y in range(len(board)):
-          if board[x][y] == 1:
-            visited[x][y] = 1
-            for idx in range(len(dx)):
-                nx = x + dx[idx]
-                ny = y + dy[idx]
-                
-                if nx >= 0 and ny >= 0 and nx < len(board) and ny < len(board) :
-                  visited[nx][ny] = 1
-
-    for x in range(len(visited)):
-      for y in range(len(visited)):
-        if visited[x][y] == 0:
-          answer += 1    
-            
+    # 방향키
+    dx = [-1, -1, -1, 0, 0, 0, 1, 1, 1]
+    dy = [-1, 0, 1, -1, 0, 1, -1, 0, 1]
+    
+    for x in range(n):
+        for y in range(n):
+            if board[x][y] == 1:
+                visited[x][y] = 1
+                for i in range(len(dx)):
+                    nx, ny = x + dx[i], y + dy[i]
+                    if 0 <= nx < n and 0 <= ny < n:
+                        visited[nx][ny] = 1
+    
+    for x in range(n):
+        for y in range(n):
+            if visited[x][y] == 0:
+                answer += 1
+    
     return answer
