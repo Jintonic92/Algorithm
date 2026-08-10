7799b2650d541f9ef7f5480600b6628ea51df40d
Jintonic92 <86962114+Jintonic92@users.noreply.github.com>
2026-08-10 14:53:32 +0900

    [level 0] Title: 안전지대, Time: 0.04 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/0/120866. 안전지대/안전지대.py
+++ 프로그래머스/0/120866. 안전지대/안전지대.py
@@ -1,21 +1,20 @@
 def solution(board):
     answer = 0
-    # 지뢰를 먼저 찾아서 주위를 다 1로 만들기 
-    # 그리고 전체 돌면서 0인 지역 찾기 
-    n = len(board)
-    visited = [[0] * n for _ in range(n)]
     
-    # 방향키
+    # 지뢰가 있는 곳 주위 1로 만들기 
+    # 없는 곳 마지막에 체크하면서 answer += 1
     dx = [-1, -1, -1, 0, 0, 0, 1, 1, 1]
     dy = [-1, 0, 1, -1, 0, 1, -1, 0, 1]
+    
+    n = len(board)
+    visited = [[0] * n for _ in range(n)]
     
     for x in range(n):
         for y in range(n):
             if board[x][y] == 1:
-                visited[x][y] = 1
                 for i in range(len(dx)):
                     nx, ny = x + dx[i], y + dy[i]
-                    if 0 <= nx < n and 0 <= ny < n:
+                    if 0 <= nx < n and 0 <= ny < n :
                         visited[nx][ny] = 1
     
     for x in range(n):
