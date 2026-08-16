04f91b8dd70ae4b0768dbe0d4236eda80bcb1244
Jintonic92 <86962114+Jintonic92@users.noreply.github.com>
2026-08-16 15:21:27 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.00 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.py
+++ 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.py
@@ -1,9 +1,13 @@
 def solution(common):
     answer = 0
-    # 등비|등차
+    
+
     if common[1] - common[0] == common[2] - common[1]:
-        answer += common[-1] + common[1] - common[0]
+        diff = common[1] - common[0]
+        answer = common[-1] + diff
+    
     else:
-        n = common[1] // common[0]
-        answer = common[-1] * n
+        diff = common[1] // common[0]
+        answer = common[-1] * diff
+
     return answer
