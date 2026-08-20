754055fb2a98f22ee3ae8a8995a1e606f8656cd5
Jintonic92 <86962114+Jintonic92@users.noreply.github.com>
2026-08-20 14:25:21 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.41 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.py
+++ 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.py
@@ -1,14 +1,17 @@
+from collections import Counter
+
 def solution(array):
+    answer = 0
+    count = Counter(array)
+
+    max_freq = count.most_common(1) # 제일 많은 것 하나 (1) 결과 [(3, 3)] 3이 3개
+    max_freq = count.most_common(1)[0] # (3, 3) 3이 3개 
+    max_freq = count.most_common(1)[0][1] # 3이 3개에서 3개를 가지고 옴
+    # print(max_freq)
     
-    visited = [0] * 1001
+    most_common_items = [i for i, f in count.items() if f == max_freq]
+    # print(most_common_items)
+    if len(most_common_items) > 1:
+        return -1
     
-    for x in array:
-        visited[x] += 1
-    
-    max_n = max(visited)
-    answer = [idx for idx, x in enumerate(visited) if x == max_n]
-    
-    if len(answer) > 1:
-        return -1 
-    else:
-        return answer[0]
+    return most_common_items[0]
