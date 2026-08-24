c90c3e798bde6afab835ae1003963869719e40f5
Jintonic92 <86962114+Jintonic92@users.noreply.github.com>
2026-08-24 15:47:54 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.00 ms, Memory: 11.7 MB -BaekjoonHub

--- 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.py
+++ 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.py
@@ -1,11 +1,8 @@
 def solution(common):
     answer = 0
-    # 등차수열
+    if common[1] - common[0] == common[2] - common[1]:
+        answer = common[-1] + common[1] - common[0]
     
-    if common[1] - common[0] == common[2] - common[1] :
-        return common[-1] + common[1] - common[0]
-    
-    else: 
-        diff = common[1] // common[0]
-        return common[0] * diff**len(common)
+    else:
+        answer = common[-1] * (common[2] // common[1]) 
     return answer
