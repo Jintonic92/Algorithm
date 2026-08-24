d0bb56414de8af15a9ff724c3a15cf5006f258a2
Jintonic92 <86962114+Jintonic92@users.noreply.github.com>
2026-08-24 16:14:18 +0900

    [level 0] Title: 코드 처리하기, Time: 6.45 ms, Memory: 12 MB -BaekjoonHub

--- 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.py
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.py
@@ -1,20 +1,19 @@
 def solution(code):
     answer = ''
-    curr_mode = '0'
-    
+    mode = 0 
     for idx, c in enumerate(code):
-        #print("current_mode", curr_mode, "idx", idx, "c", c)
-        if c == '1':
-            curr_mode = '1' if curr_mode == '0' else '0'
-            continue
-            
-        if curr_mode == '0' :
-            if idx % 2 == 0 :
-                answer += c
-        else:
-            if idx % 2 == 1 :
-                answer += c
+        if mode == 0 :
+            if c != "1" :
+                if idx % 2 == 0:
+                    answer += c
+            else:
+                mode = 1
+        elif mode == 1:
+            if c != "1":
+                if idx % 2 == 1:
+                    answer += c
+            else:
+                mode = 0
     if len(answer) == 0:
         return "EMPTY"
     return answer
-
