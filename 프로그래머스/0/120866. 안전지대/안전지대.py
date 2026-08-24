ae75cd473677639ce73c64dc6a175dd6e458a49e
Jintonic92 <86962114+Jintonic92@users.noreply.github.com>
2026-08-24 15:39:47 +0900

    [level 0] Title: 안전지대, Time: 0.04 ms, Memory: 11.7 MB -BaekjoonHub

--- 프로그래머스/0/120866. 안전지대/안전지대.py
+++ 프로그래머스/0/120866. 안전지대/안전지대.py
@@ -1,24 +1,22 @@
 def solution(board):
     answer = 0
-    
-    # 지뢰이면 주위 1로 바꿈
-    n = len(board)
-    visited = [[0] * n for _ in range(n)]
-    
+    # 닿는 곳 1로 바꾸기
     dx = [-1, -1, -1, 0, 0, 0, 1, 1, 1]
     dy = [-1, 0, 1, -1, 0, 1, -1, 0, 1]
     
-    for x in range(n):
-        for y in range(n):
-            if board[x][y]  == 1:
-                for i in range(len(dx)):
-                    nx = x + dx[i]
-                    ny = y + dy[i]
-                    if 0 <= nx < n and 0 <= ny < n and not visited[nx][ny] :
-                        visited[nx][ny] = 1
+    visited = [[0] * len(board) for _ in range(len(board))]
+    n = len(board)
     
     for x in range(n):
         for y in range(n):
-            if not visited[x][y] :
-                answer += 1
+            if board[x][y] == 1:
+                for i in range(len(dx)):
+                    nx, ny = x + dx[i], y + dy[i]
+                    if 0 <= nx < n and 0 <= ny < n and not visited[nx][ny]:
+                        visited[nx][ny] = 1
+    for x in range(n):
+        for y in range(n):
+            if not visited[x][y]:
+                answer += 1 
+                
     return answer
