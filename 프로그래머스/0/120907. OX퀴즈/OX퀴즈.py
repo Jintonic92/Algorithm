7dd9e8c8cfdce5d01cfdf90faecfa5ce2c3987f6
Jintonic92 <86962114+Jintonic92@users.noreply.github.com>
2026-08-24 15:50:20 +0900

    [level 0] Title: OX퀴즈, Time: 0.01 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/0/120907. OX퀴즈/OX퀴즈.py
+++ 프로그래머스/0/120907. OX퀴즈/OX퀴즈.py
@@ -2,11 +2,10 @@ def solution(quiz):
     answer = []
     for q in quiz:
         x, op, y, eq, z = q.split()
-        
-        if op == '-':
-            answer.append("O" if int(x) - int(y) == int(z) else "X")
-
+        x, y, z = int(x), int(y), int(z)
+    
+        if op == '+':
+            answer.append( "O" if x + y == z else "X")
         else:
-            answer.append("O" if int(x) + int(y) == int(z) else "X")
-
+            answer.append("O" if x - y == z else "X")
     return answer
