f1853a860fd84e3279c04c26e42b6dbb6534240b
Jintonic92 <86962114+Jintonic92@users.noreply.github.com>
2026-08-25 11:20:34 +0900

    [level 0] Title: 특이한 정렬, Time: 0.03 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.py
+++ 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.py
@@ -1,16 +1,9 @@
 def solution(numlist, n):
     answer = []
     a_list = []
-    
     for num in numlist:
-        # [원소 값, n과의 거리] 형태로 저장
-        a_list.append([num, abs(n - num)])
-        
-    # 1순위: 거리(x[1])가 가까운 순(오름차순)
-    # 2순위: 원소 값(x[0])이 큰 순(내림차순, 앞에 마이너스 부호 붙이기)
-    a_list = sorted(a_list, key=lambda x: (x[1], -x[0]))
-    
-    for v, dist in a_list:
+        a_list.append([num, abs(n-num)])
+    a_list = sorted(a_list, key = lambda x : (x[1], -x[0]))
+    for v, diff in a_list:
         answer.append(v)
-        
     return answer
