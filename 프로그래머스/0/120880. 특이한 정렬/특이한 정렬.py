6ac899bf7630e76fc51a6a39a6744359db0e46f7
Jintonic92 <86962114+Jintonic92@users.noreply.github.com>
2026-08-25 21:26:42 +0900

    [level 0] Title: 특이한 정렬, Time: 0.03 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.py
+++ 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.py
@@ -2,8 +2,9 @@ def solution(numlist, n):
     answer = []
     a_list = []
     for num in numlist:
-        a_list.append([num, abs(n-num)])
+        a_list.append([num, abs(num - n)])
     a_list = sorted(a_list, key = lambda x : (x[1], -x[0]))
-    for v, diff in a_list:
-        answer.append(v)
+    
+    for num, diff in a_list:
+        answer.append(num)
     return answer
