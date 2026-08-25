3a8ac7aa169c0ec68dcbd4e3aeb8dc1d94d6f5ff
Jintonic92 <86962114+Jintonic92@users.noreply.github.com>
2026-08-25 11:07:35 +0900

    [level 0] Title: 주사위 게임 3, Time: 0.01 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.py
+++ 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.py
@@ -2,33 +2,29 @@ def solution(a, b, c, d):
     answer = 0
     a_list = [a, b, c, d]
     visited = [0] * 7
-    for x in a_list:
-        visited[x] += 1
-        
-    # 4개 다 같다면 
-    if max(visited) == 4:
-        return 1111 * a
+    for i in a_list:
+        visited[i] += 1
     
-    # 4개 다 다르다면 visited max 가 1
-    if max(visited) == 1:
-        return min(a_list)
+    if len(set(a_list)) == 1:
+        answer = 1111 * a
     
-    # 2개만 같고 나머지가 다르다면
-    if len(set(a_list)) == 3:
-        q = [idx for idx, x in enumerate(visited) if x == 1][0]
-        r = [idx for idx, x in enumerate(visited) if x == 1][1]
-        return q * r
-    
-    # 3개가 같고 나머지가 다르다면
-    if max(visited) == 3:
+    elif max(visited) == 3:
         p = [idx for idx, x in enumerate(visited) if x == 3][0]
         q = [idx for idx, x in enumerate(visited) if x == 1][0]
-        return (10 * p + q)**2
+        answer = ( 10 * p + q)**2
     
-    # 2개 쌍일 경우
-    if len(set(a_list)) == 2:
+    elif len(set(a_list)) == 2:
         p = [idx for idx, x in enumerate(visited) if x == 2][0]
         q = [idx for idx, x in enumerate(visited) if x == 2][1]
-        return (p + q) * abs(p - q)
+        answer = (p + q) * abs(p - q)
+        
+    elif max(visited) == 1:
+        answer = min(a_list)
     
+    else:
+        p = [idx for idx, x in enumerate(visited) if x == 2][0]
+        q = [idx for idx, x in enumerate(visited) if x == 1][0]
+        r = [idx for idx, x in enumerate(visited) if x == 1][1]
+        answer = q * r
+
     return answer
