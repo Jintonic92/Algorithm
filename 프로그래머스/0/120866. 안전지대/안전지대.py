19119906680e265e8903e00d1037639f14011576
Jintonic92 <86962114+Jintonic92@users.noreply.github.com>
2026-08-28 15:23:56 +0900

    [level 0] Title: 안전지대, Time: 0.04 ms, Memory: 11.8 MB -BaekjoonHub

--- 프로그래머스/0/120866. 안전지대/안전지대.py
+++ 프로그래머스/0/120866. 안전지대/안전지대.py
@@ -1,22 +1,22 @@
 def solution(board):
     answer = 0
-    # 닿는 곳 1로 바꾸기
+    # 닿는 곳은 모두 폭파 지역 
     dx = [-1, -1, -1, 0, 0, 0, 1, 1, 1]
     dy = [-1, 0, 1, -1, 0, 1, -1, 0, 1]
-    
-    visited = [[0] * len(board) for _ in range(len(board))]
     n = len(board)
+    a_list = [[0] * n for _ in range(n)]
     
     for x in range(n):
         for y in range(n):
+            
             if board[x][y] == 1:
                 for i in range(len(dx)):
                     nx, ny = x + dx[i], y + dy[i]
-                    if 0 <= nx < n and 0 <= ny < n and not visited[nx][ny]:
-                        visited[nx][ny] = 1
+                    if 0 <= nx < n and 0 <= ny < n :
+                        a_list[nx][ny] = 1
+                    
     for x in range(n):
         for y in range(n):
-            if not visited[x][y]:
-                answer += 1 
-                
+            if not a_list[x][y]:
+                answer += 1
     return answer
