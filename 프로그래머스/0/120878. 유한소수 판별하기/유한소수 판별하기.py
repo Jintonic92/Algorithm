9c3427cd2f42edfb66a3a084125205849e86e704
Jintonic92 <86962114+Jintonic92@users.noreply.github.com>
2026-08-31 22:58:31 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.02 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.py
+++ 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.py
@@ -1,28 +1,26 @@
 def gcd(a, b):
     a, b = max(a, b), min(a, b)
-    while b != 0 :
-        a, b = b, a % b 
-    return a
-
+    while b > 0 :
+        a, b = b, a % b
+    return a 
 
 def solution(a, b):
-
+    answer = 0
+    gcd_n = gcd(a, b)
+    numer = b // gcd_n 
     
-    num, denom = a // gcd(a, b), b // gcd(a, b)
-    
-    while denom > 1:
- 
-        if denom % 2 == 0:
-            denom //= 2
+    while numer > 1 :
+        if numer % 2 == 0:
+            numer //= 2
         
-        elif denom % 5 == 0:
-            denom //= 5
+        elif numer % 5 == 0:
+            numer //= 5
         else:
-            break 
-            
-    if denom == 1:
+            break
+    
+    if numer == 1:
         return 1
     else:
         return 2
-        
     
+    return answer
