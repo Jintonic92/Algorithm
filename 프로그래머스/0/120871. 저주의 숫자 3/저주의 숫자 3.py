96f33b5e9d371baac99154dde1ba8c67c8f223f4
Jintonic92 <86962114+Jintonic92@users.noreply.github.com>
2026-08-31 23:07:51 +0900

    [level 0] Title: 저주의 숫자 3, Time: 0.02 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/0/120871. 저주의 숫자 3/저주의 숫자 3.py
+++ 프로그래머스/0/120871. 저주의 숫자 3/저주의 숫자 3.py
@@ -2,10 +2,9 @@ def solution(n):
     answer = 0
     for _ in range(n):
         answer += 1
-        # 3의 배수 | '3'이 포함되어 있으면
+        
         while answer % 3 == 0 or '3' in str(answer):
             answer += 1
-    
+        
+            
     return answer
-
-
