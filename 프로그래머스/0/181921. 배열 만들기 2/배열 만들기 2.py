c5511ebaf526272b88e2490250c55e5124ce5142
Jintonic92 <86962114+Jintonic92@users.noreply.github.com>
2026-09-01 18:12:02 +0900

    [level 0] Title: 배열 만들기 2, Time: 95.43 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.py
+++ 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.py
@@ -1,14 +1,12 @@
 def solution(l, r):
     answer = []
-    
     for i in range(l, r+1):
         str_i = str(i)
-        str_i = str_i.replace("5", " ")
-        str_i = str_i.replace("0", " ")
+        str_i = str_i.replace('5', '')
+        str_i = str_i.replace('0', '')
         
-        if str_i.strip() == "":
+        if str_i.strip() == '':
             answer.append(i)
-    
     if len(answer) == 0:
         return [-1]
     return answer
