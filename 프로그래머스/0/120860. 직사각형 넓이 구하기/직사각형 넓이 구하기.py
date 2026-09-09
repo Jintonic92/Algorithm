a5443a09615d61f36fe79ce95941395358d8a571
Jintonic92 <86962114+Jintonic92@users.noreply.github.com>
2026-09-09 15:29:33 +0900

    [level 0] Title: 직사각형 넓이 구하기, Time: 0.01 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/0/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.py
+++ 프로그래머스/0/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.py
@@ -1,11 +1,12 @@
 def solution(dots):
     answer = 0
-    x_list = [x for x, y in dots]
-    y_list = [y for x, y in dots]
-
-    w = max(x_list) - min(x_list)
-    h = max(y_list) - min(y_list)
-     
-    answer = w * h
+    x_list, y_list = [], []
+    for x, y in dots:
+        x_list.append(x)
+        y_list.append(y)
     
+    max_x, min_x = max(x_list), min(x_list)
+    max_y, min_y = max(y_list), min(y_list)
+    
+    answer = (max_x - min_x) * (max_y - min_y)
     return answer
