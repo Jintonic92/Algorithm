0394d35229639ad316505547955adbe72c4cde3c
Jintonic92 <86962114+Jintonic92@users.noreply.github.com>
2026-09-09 15:32:22 +0900

    [level 0] Title: 등수 매기기, Time: 0.01 ms, Memory: 11.7 MB -BaekjoonHub

--- 프로그래머스/0/120882. 등수 매기기/등수 매기기.py
+++ 프로그래머스/0/120882. 등수 매기기/등수 매기기.py
@@ -1,11 +1,15 @@
 def solution(score):
     answer = []
-    a_list = [x + y  for x, y in score]
-    for x in a_list:
-        rank = 1 
-        for y in a_list:
-            if x < y :
+    a_list = []
+    for x, y in score:
+        z = (x + y) / 2
+        a_list.append(z)
+    
+    for z in a_list:
+        rank = 1
+        for z_c in a_list:
+            if z < z_c :
                 rank += 1
         answer.append(rank)
-    print(answer)
+        
     return answer
